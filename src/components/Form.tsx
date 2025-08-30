988a7ead8aa020c2be841515ebe6e8b6953f43e1
pedroholguin <dropeholguin1994@gmail.com>
2025-08-30 09:06:48 -0500

    Form and list of activities are added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,27 @@
-import { useState, type ChangeEvent } from "react";
+import {
+  useState,
+  type ChangeEvent,
+  type Dispatch,
+  type FormEvent,
+} from "react";
+import { v4 as uuidv4 } from 'uuid'
 import { categories } from "../data/categories";
 import type { Activity } from "../types";
+import type { ActivityActions } from "../reducers/activity-reducer";
 
-export default function Form() {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+};
+
+const initalState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
+export default function Form({ dispatch }: FormProps) {
+  const [activity, setActivity] = useState<Activity>(initalState);
 
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement | HTMLInputElement>
@@ -25,9 +39,26 @@ export default function Form() {
     return name.trim() !== "" && calories > 0;
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!isValidActivity()) return;
+
+    dispatch({
+      type: "save-activity",
+      payload: { newActivity: activity },
+    });
+
+    setActivity(
+      { ...initalState, id: uuidv4() } // Reset state and generate a new id
+    );
+  };
 
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
           Categoría:
